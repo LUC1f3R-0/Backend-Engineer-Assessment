4e22b823e10a434554facd364e0b9ced71f9cf96
LUC1f3R-0 <thusharathilan123@gmail.com>
2026-03-29 14:27:00 +0530

    chore: add NotFound route to handle undefined paths in the application

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -1,6 +1,7 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import Products from './pages/Products';
 import Order from './pages/Order';
+import NotFound from './pages/error/NotFound';
 
 export function App() {
   return (
@@ -8,6 +9,8 @@ export function App() {
       <Route path="/" element={<Navigate to="/products" replace />} />
       <Route path="/products" element={<Products />} />
       <Route path="/orders" element={<Order />} />
+
+      <Route path='*' element={<NotFound/>}/>
     </Routes>
   );
 }

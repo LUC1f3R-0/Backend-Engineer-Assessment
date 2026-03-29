64ed33ab1ae014cf53510749412dabdba5c6661f
LUC1f3R-0 <thusharathilan123@gmail.com>
2026-03-29 13:24:08 +0530

    chore: add dotenv configuration and update tsconfig.app.json to remove ignoreDeprecations option

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -6,6 +6,8 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import * as dotenv from "dotenv";
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,7 +16,7 @@ async function bootstrap() {
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
+    `🚀 Application is running on ${process.env.NODE_ENV}: http://localhost:${port}/${globalPrefix}`,
   );
 }
 

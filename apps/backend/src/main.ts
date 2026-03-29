c40e50a078853acb82a939e146e98c94a81eeb67
LUC1f3R-0 <thusharathilan123@gmail.com>
2026-03-30 02:53:12 +0530

    refactor: remove unused AppController and AppService; add new interceptor and config files

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -11,6 +11,7 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;

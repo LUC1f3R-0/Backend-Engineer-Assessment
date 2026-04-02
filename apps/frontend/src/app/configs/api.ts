13dcf2f77d7bc0b92d0f1994acaf327a5b362914
LUC1f3R-0 <thusharathilan123@gmail.com>
2026-04-02 17:52:33 +0530

    fix: update backend URL to a specific production endpoint and add additional console log for debugging

--- apps/frontend/src/app/configs/api.ts
+++ apps/frontend/src/app/configs/api.ts
@@ -1,10 +1,9 @@
 import axios from "axios";
 
-const backendUrl =
-  import.meta.env.VITE_BACKEND_URL ||
-  (import.meta.env.DEV ? 'http://localhost:3000' : '');
+const backendUrl = "https://backend-engineer-assessment-534959216168.asia-south1.run.app" ?? "http://localhost:3000";
 
-console.log(backendUrl);
+console.log(backendUrl)
+console.log("this is running second")
 
 const axiosInstance = axios.create({
   baseURL: backendUrl,
@@ -13,7 +12,6 @@ const axiosInstance = axios.create({
     'Content-Type': 'application/json',
     'x-api-key': import.meta.env.VITE_X_API_KEY as string,
   },
-});
-
+})
 
 export default axiosInstance;

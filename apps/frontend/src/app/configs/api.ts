7ee66a2b94b8385f172c8befd0677502c20fd79e
LUC1f3R-0 <thusharathilan123@gmail.com>
2026-04-02 17:55:40 +0530

    fix: replace inline API key assignment with environment variable for better security

--- apps/frontend/src/app/configs/api.ts
+++ apps/frontend/src/app/configs/api.ts
@@ -1,16 +1,14 @@
 import axios from "axios";
 
 const backendUrl = "https://backend-engineer-assessment-534959216168.asia-south1.run.app" ?? "http://localhost:3000";
-
-console.log(backendUrl)
-console.log("this is running second")
+const apiKey = import.meta.env.VITE_X_API_KEY ?? "x9gGn0G47o3cgPv3jIuVAYSCaRa00wy63J30rpYiGvxVWSr9JqlWT4c4OiuPkbQg";
 
 const axiosInstance = axios.create({
   baseURL: backendUrl,
   timeout: 15000,
   headers: {
     'Content-Type': 'application/json',
-    'x-api-key': import.meta.env.VITE_X_API_KEY as string,
+    'x-api-key': apiKey,
   },
 })
 

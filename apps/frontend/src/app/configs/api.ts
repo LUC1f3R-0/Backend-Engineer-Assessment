465bf24aa2a9fb8428f3bf35b5eb12e11b5d516a
LUC1f3R-0 <thusharathilan123@gmail.com>
2026-04-02 18:12:19 +0530

    chore: remove console logs from api.ts for cleaner code

--- apps/frontend/src/app/configs/api.ts
+++ apps/frontend/src/app/configs/api.ts
@@ -3,9 +3,6 @@ import axios from "axios";
 const backendUrl = import.meta.env.VITE_BACKEND_URL ?? "http://localhost:3000";
 const apiKey = import.meta.env.VITE_X_API_KEY as string;
 
-console.log(backendUrl)
-console.log(apiKey)
-
 const axiosInstance = axios.create({
   baseURL: backendUrl,
   timeout: 15000,

8e4ecb7146f3630a5632e19fc03b8872517446b3
LUC1f3R-0 <thusharathilan123@gmail.com>
2026-04-02 18:07:14 +0530

    fix: update Netlify deployment configuration to include backend URL and API key as environment variables

--- apps/frontend/src/app/configs/api.ts
+++ apps/frontend/src/app/configs/api.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 
-const backendUrl = "https://backend-engineer-assessment-534959216168.asia-south1.run.app" ?? "http://localhost:3000";
-const apiKey = import.meta.env.VITE_X_API_KEY ?? "x9gGn0G47o3cgPv3jIuVAYSCaRa00wy63J30rpYiGvxVWSr9JqlWT4c4OiuPkbQg";
+const backendUrl = import.meta.env.VITE_BACKEND_URL ?? "http://localhost:3000";
+const apiKey = import.meta.env.VITE_X_API_KEY as string;
+
+console.log(backendUrl)
+console.log(apiKey)
 
 const axiosInstance = axios.create({
   baseURL: backendUrl,
